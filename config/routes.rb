1be5e8bef453253361f6c5ba5f4c68e85492f291
chepyego <jchepyego@gmail.com>
2023-02-21 15:39:10 +0300

    fix root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :products
+  
+  namespace :api do 
+    namespace :v1 do
+      resources :products
+
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  root "products#index"
+  root "api/v1/products#index"
 end
