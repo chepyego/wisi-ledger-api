a515b36a7190c02f66aff897b7e9ff5021806a6b
chepyego <jchepyego@gmail.com>
2023-02-21 15:01:05 +0300

    initial setup

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @product = products(:one)
+  end
+
+  test "should get index" do
+    get products_url, as: :json
+    assert_response :success
+  end
+
+  test "should create product" do
+    assert_difference("Product.count") do
+      post products_url, params: { product: { image_url: @product.image_url, price: @product.price, title: @product.title } }, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should show product" do
+    get product_url(@product), as: :json
+    assert_response :success
+  end
+
+  test "should update product" do
+    patch product_url(@product), params: { product: { image_url: @product.image_url, price: @product.price, title: @product.title } }, as: :json
+    assert_response :success
+  end
+
+  test "should destroy product" do
+    assert_difference("Product.count", -1) do
+      delete product_url(@product), as: :json
+    end
+
+    assert_response :no_content
+  end
+end
